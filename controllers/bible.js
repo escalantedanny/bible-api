a235d52787d4c868b2ee8431ec60c0824667f268
Danny Escalante <danny.dev@developer.dev>
2025-04-22 09:24:03 -0400

    refactoring with controller and reouter files

---
+++ controllers/bible.js
@@ -0,0 +1,117 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Cargar el JSON de la Biblia
+const rawData = fs.readFileSync(path.resolve(__dirname, '../db/biblia.json'), 'utf-8');
+const originalBiblia = JSON.parse(rawData);
+
+// Normalizar las claves a minúsculas
+const biblia = {};
+for (const key in originalBiblia) {
+  biblia[key.toLowerCase()] = originalBiblia[key];
+}
+
+export async function getBooks(req, res) {
+    const libros = Object.keys(biblia);
+    if (libros.length === 0) {
+        return res.status(404).json({ error: 'No se encontraron libros' });
+    }
+    res.json(libros);
+}
+
+export async function getBook(req, res) {
+    const libro = req.params.libro.toLowerCase();
+    const contenido = biblia[libro];
+
+    if (contenido) {
+        res.json(contenido);
+    } else {
+        res.status(404).json({ error: 'Libro no encontrado' });
+    }
+}
+
+export async function getChapters(req, res) {
+    const libro = req.params.libro.toLowerCase();
+    const contenido = biblia[libro];
+  
+    if (contenido && contenido.chapters) {
+      const capitulos = contenido.chapters.map(chapter => chapter.chapter);
+      console.log(capitulos);
+      
+      res.json(capitulos);
+    } else {
+      res.status(404).json({ error: 'Capítulos no encontrados' });
+    }
+}
+
+export async function getChapter(req, res) {
+    const libro = req.params.libro.toLowerCase();
+    const capitulo = req.params.capitulo;
+  
+    const contenido = biblia[libro];
+    if (contenido && contenido.chapters) {
+      const chapter = contenido.chapters.find(ch => ch.chapter === capitulo);
+      if (chapter) {
+        res.json(chapter);
+      } else {
+        res.status(404).json({ error: 'Capítulo no encontrado' });
+      }
+    } else {
+      res.status(404).json({ error: 'Libro o capítulos no encontrados' });
+    }
+}
+
+export async function getVersicles(req, res) {
+    const libro = req.params.libro.toLowerCase();
+    const capitulo = req.params.capitulo;
+    const contenido = biblia[libro];
+  
+    // Obtener los versículos solicitados desde el parámetro query
+    const versiculosSolicitados = req.query.versiculos ? req.query.versiculos.split(',') : [];
+  
+    if (contenido && contenido.chapters) {
+      const capítulo = contenido.chapters.find(ch => ch.chapter === capitulo);
+      if (capítulo && capítulo.verses) {
+        // Si se han solicitado versículos específicos, devolver solo esos
+        if (versiculosSolicitados.length > 0) {
+          const versiculos = {};
+          versiculosSolicitados.forEach(num => {
+            if (capítulo.verses[num]) {
+              versiculos[num] = capítulo.verses[num];
+            }
+          });
+          res.json(versiculos);
+        } else {
+          // Si no se han solicitado versículos específicos, devolver todos los versículos del capítulo
+          res.json(capítulo.verses);
+        }
+      } else {
+        res.status(404).json({ error: 'Versículos no encontrados en este capítulo' });
+      }
+    } else {
+      res.status(404).json({ error: 'Capítulo no encontrado' });
+    }
+}
+
+export async function getVersicle(req, res) {
+    const libro = req.params.libro.toLowerCase();
+    const capitulo = req.params.capitulo;
+    const versiculo = req.params.versiculo;
+  
+    const contenido = biblia[libro];
+    if (contenido && contenido.chapters) {
+      const chapter = contenido.chapters.find(ch => ch.chapter === capitulo);
+      if (chapter && chapter.verses && chapter.verses[versiculo]) {
+        res.json({ [versiculo]: chapter.verses[versiculo] });
+      } else {
+        res.status(404).json({ error: 'Versículo no encontrado' });
+      }
+    } else {
+      res.status(404).json({ error: 'Libro, capítulo o versículo no encontrados' });
+    }
+}
---
+++ routes/bible.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { 
+    getBooks, 
+    getBook,
+    getChapters,
+    getChapter,
+    getVersicles,
+    getVersicle 
+} from '../controllers/bible.js';
+
+const router = express.Router();
+
+// Ruta para obtener todos los libros
+router.get('/', getBooks);
+
+// Ruta para obtener un libro específico (por ejemplo, "genesis")
+router.get('/:libro', getBook)
+
+// Ruta para obtener capítulos de un libro (por ejemplo, "genesis")
+router.get('/:libro/capitulos', getChapters)
+
+// Ruta para obtener un capítulo específico de un libro (por ejemplo, "genesis" y "1")
+router.get('/:libro/capitulos/:capitulo', getChapter)
+
+// Ruta para obtener múltiples versículos de un capítulo de un libro (por ejemplo, "genesis/1")
+router.get('/:libro/capitulos/:capitulo/versiculos', getVersicles)
+
+// Ruta para obtener un versículo específico de un libro y capítulo (por ejemplo, "genesis", "1", "1")
+router.get('/:libro/capitulos/:capitulo/versiculos/:versiculo', getVersicle)
+
+export { router as bibleRouter };
